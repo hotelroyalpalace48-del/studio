c3b162bb83da281c529695463eecddbe99c13dd5
HOTEL ROYAL PALACE <hotelroyalpalace48@gmail.com>
2026-03-30 11:48:29 +0000

    Set up a Firebase backend

---
+++ src/firebase/config.ts
@@ -0,0 +1,8 @@
+export const firebaseConfig = {
+  "projectId": "studio-2854938285-75257",
+  "appId": "1:340033517245:web:666b8593864c7d50d8e9d4",
+  "apiKey": "AIzaSyAa9L3Ese6uF5Wbws21ddc_hQi6Ap6sQ88",
+  "authDomain": "studio-2854938285-75257.firebaseapp.com",
+  "measurementId": "",
+  "messagingSenderId": "340033517245"
+};
